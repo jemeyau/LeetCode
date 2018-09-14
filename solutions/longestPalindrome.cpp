8be53471b1a028771d36da115081591b1d6eafeb
jem <youjh1990@gmail.com>
2018-09-14 21:21:33 +0800

    add "Longest Palindromic Substring"

---
+++ solutions/longestPalindrome.cpp
@@ -0,0 +1,86 @@
+/*
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+Input: "cbbd"
+Output: "bb"
+*/
+
+#include "comm_header.h"
+
+class Solution
+{
+  public:
+    string longestPalindrome(string s)
+    {
+    }
+};
+
+string stringToString(string input)
+{
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() - 1; i++)
+    {
+        char currentChar = input[i];
+        if (input[i] == '\\')
+        {
+            char nextChar = input[i + 1];
+            switch (nextChar)
+            {
+            case '\"':
+                result.push_back('\"');
+                break;
+            case '/':
+                result.push_back('/');
+                break;
+            case '\\':
+                result.push_back('\\');
+                break;
+            case 'b':
+                result.push_back('\b');
+                break;
+            case 'f':
+                result.push_back('\f');
+                break;
+            case 'r':
+                result.push_back('\r');
+                break;
+            case 'n':
+                result.push_back('\n');
+                break;
+            case 't':
+                result.push_back('\t');
+                break;
+            default:
+                break;
+            }
+            i++;
+        }
+        else
+        {
+            result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        string s = stringToString(line);
+
+        string ret = Solution().longestPalindrome(s);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}

4bbdced6f9b65c8dd9ec8344313a4b958685a151
jem <youjh1990@gmail.com>
2018-10-10 10:33:07 +0800

    still time limit exceeded

--- solutions/threeSum.cpp
+++ solutions/threeSum.cpp
@@ -16,53 +16,89 @@ A solution set is:
 
 #include "comm_header.h"
 
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+#include <map>
+
+class Solution
+{
+  public:
+    struct node
+    {
+        int first;
+        int second;
+    };
+
+  public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
         sort(nums.begin(), nums.end());
+
+        map<int, vector<node>> store;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                node tmp_node;
+                tmp_node.first = i;
+                tmp_node.second = j;
+
+                int sum = nums[i] + nums[j];
+
+                store[sum].push_back(tmp_node);
+            }
+        }
 
         vector<vector<int>> ret;
 
         for (int i = 0; i < nums.size(); i++)
         {
             vector<int> tmp;
-            int i_want = 0 - nums[i];
+            int want = 0 - nums[i];
             tmp.push_back(nums[i]);
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                int j_want = i_want - nums[j];
-                tmp.push_back(nums[j]);
-                for (int k = j + 1; k < nums.size(); k++)
-                {
-                    if (nums[k] == j_want)
-                    {
-                        tmp.push_back(nums[k]);
-                        bool exist = false;
-                        for (int m = 0; m < ret.size(); m++)
-                        {
-                            vector<int>& a_ret = ret[m];
 
-                            if (a_ret[0] == nums[i] && a_ret[1] == nums[j] && a_ret[2] == nums[k])
-                            {
-                                exist = true;
-                                break;
-                            }
-                        }
-                        if (!exist)
-                            ret.push_back(tmp);
-                        tmp.pop_back();
+            if (store.find(want) == store.end())
+                continue;
+
+            vector<node>& node_list = store.find(want)->second;
+
+            for (int j = 0; j < node_list.size(); j++)
+            {
+                tmp.clear();
+                node& one_node = node_list[j];
+                if (one_node.first == i || one_node.second == i)
+                    break;
+
+                cout << "a: " << nums[i] << " " << nums[one_node.first] << " " << nums[one_node.second] << endl;
+
+                tmp.push_back(nums[i]);
+                tmp.push_back(nums[one_node.first]);
+                tmp.push_back(nums[one_node.second]);
+
+                cout << "c: " << tmp[0] << " " << tmp[1] << " " << tmp[2] << endl;
+
+                sort(tmp.begin(), tmp.end());
+
+                bool exist = false;
+                for (int m = 0; m < ret.size(); m++)
+                {
+                    vector<int> &a_ret = ret[m];
+
+                    if (a_ret[0] == tmp[0] && a_ret[1] == tmp[1] && a_ret[2] == tmp[2])
+                    {
+                        exist = true;
                         break;
                     }
                 }
-                tmp.pop_back();
+                if (!exist)
+                {
+                    ret.push_back(tmp);
+                    cout << "b: " << tmp[0] << " " << tmp[1] << " " << tmp[2] << endl;
+                }
             }
-            tmp.pop_back();
         }
 
         return ret;
     }
 };
-
 
 void trimLeftTrailingSpaces(string &input)
 {

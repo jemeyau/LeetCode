876c16e7524e4d629f0b2e61d71dc7bdefe83e63
youjh90 <jemeyau@gmail.com>
2019-05-16 22:01:28 +0800

    Best Time to Buy and Sell Stock, O(n)

--- solutions/maxProfit.cpp
+++ solutions/maxProfit.cpp
@@ -5,15 +5,15 @@ class Solution
 public:
     int maxProfit(vector<int> &prices)
     {
+        if (0 == prices.size())
+            return 0;
         int maxProfit = 0;
+        int minPrice = prices[0];
 
-        for (int i = 0; i < prices.size(); i++)
+        for (int i = 1; i < prices.size(); i++)
         {
-            for (int j = i + 1; j < prices.size(); j++)
-            {
-                if (prices[j] - prices[i] > maxProfit)
-                    maxProfit = prices[j] - prices[i];
-            }
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
         }
         
         return maxProfit;
